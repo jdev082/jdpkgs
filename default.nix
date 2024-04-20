31c3f34e26dce05e5542817ae75fd75b1aacf898
jdev082 <jdev082@jdev.eu.org>
2024-04-20 13:54:23 -0500

    feat(repo): add nx-utils
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
+  nx-utils = pkgs.callPackage ./pkgs/nx-utils { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }

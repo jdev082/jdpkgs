e9e8a9b6355b5fb76f595b0f58363bac1dd3e4f0
jdev082 <jdev082@jdev.eu.org>
2024-04-20 15:55:29 -0500

    fix(pkgs): adjust build output paths of nx-utils
    Signed-off-by: jdev082 <jdev082@jdev.eu.org>

--- pkgs/nx-utils/default.nix
+++ pkgs/nx-utils/default.nix
@@ -5,9 +5,9 @@ stdenv.mkDerivation rec {
   version = "1.0";
   src = ./.;
   installPhase = ''
-    mkdir -p $out
-    install -Dm755 nxc-autobk.sh $out/nxc-autobk
-    install -Dm755 nxc-bkdiff.sh $out/nxc-bkdiff
+    mkdir -p $out/bin
+    install -Dm755 nxc-autobk.sh $out/bin/nxc-autobk
+    install -Dm755 nxc-bkdiff.sh $out/bin/nxc-bkdiff
   '';
   meta = with lib; {
     description = "Additional utilities for working with a NixOS system.";
